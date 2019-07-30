a78ff3a3a51e914baa44a5b81e22196740a635b0
Uday Bhaskar Sarma Seetamraju <ToSarma@gmail.com>
2019-07-30 14:17:09 -0400

    placeholder.  Everything is commented out

---
+++ src/main/java/org/ASUX/common/module-info.java
@@ -0,0 +1,48 @@
+/*
+ BSD 3-Clause License
+ 
+ Copyright (c) 2019, ToSarma@gmail.com
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ 
+ * Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+ 
+ * Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+ 
+ * Neither the name of the copyright holder nor the names of its
+ contributors may be used to endorse or promote products derived from
+ this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+ // see https://openjdk.java.net/projects/jigsaw/quick-start
+module org.asux.common {
+    // exports org.asux.common;
+    // requires package.com;
+    // uses org.junit;
+    // provides org.junit with org.junit.vintage;
+    // see https://openjdk.java.net/projects/jigsaw/quick-start
+}
+
+// UNABLE TO SOLVE THE JAVADOC problem:-
+// [ERROR] Failed to execute goal org.apache.maven.plugins:maven-javadoc-plugin:3.1.0:javadoc (default-cli) on project common: An error has occurred in Javadoc report generation: 
+// [ERROR] Exit code: 1 - /Users/Sarma/Documents/Development/src/org.ASUX/org.ASUX.common/src/main/java/org/ASUX/common/Macros.java:40: error: package org.junit is not visible
+// [ERROR] import static org.junit.Assert.*;
+
+//EOF
